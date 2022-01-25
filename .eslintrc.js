eb6e02525ca90b734e828c8d8a0e33120192efb2
Tanel Puström <tanel.pustrom@verkkokauppa.com>
2022-01-25 13:04:05 +0200

    chore: add redux and dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,7 +15,7 @@ module.exports = {
   },
   plugins: ['react', '@typescript-eslint'],
   rules: {
-    indent: ['error', 2],
+    indent: ['warn', 2, { SwitchCase: 1 }],
     'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single'],
     semi: ['error', 'never'],
