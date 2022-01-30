7bb453788391191a32b22450e7bcad3882e9369d
Tanel Puström <tanel.pustrom@verkkokauppa.com>
2022-01-30 15:00:58 +0200

    test: add App.tsx test

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -1,8 +1,39 @@
 import { render, screen } from '@testing-library/react'
+import { Provider } from 'react-redux'
 import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />)
-  const linkElement = screen.getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
+import { MemoryRouter } from 'react-router-dom'
+import { getMockStore } from '../tests/mockStore'
+
+describe('<App />', () => {
+  it('should render login view', () => {
+    const store = getMockStore({})
+
+    render(
+      <Provider store={store}>
+        <MemoryRouter initialEntries={['/']}>
+          <App />
+        </MemoryRouter>
+      </Provider>
+    )
+
+    expect(screen.getByRole('heading', { name: /LOGIN/ })).toBeInTheDocument()
+    expect(screen.getByRole('textbox', { name: /Email/ })).toBeInTheDocument()
+    expect(screen.getByRole('textbox', { name: /Name/ })).toBeInTheDocument()
+  })
+
+  it('should render main', () => {
+    const store = getMockStore({ posts: { isLoading: true }, user: { sl_token: 'sl_token' } })
+
+    render(
+      <Provider store={store}>
+        <MemoryRouter initialEntries={['/']}>
+          <App />
+        </MemoryRouter>
+      </Provider>
+    )
+
+    expect(screen.queryByRole('heading', { name: /LOGIN/ })).not.toBeInTheDocument()
+    expect(screen.getByRole('main')).toBeInTheDocument()
+  })
 })
